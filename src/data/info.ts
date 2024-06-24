0583f5979c4aab1751876d72b0f257a1dc2f181c
alvarosrt <alvarosalort@hotmail.com.ar>
2024-06-24 12:47:47 -0300

    Add files via upload

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,88 +1,98 @@
 export const info = {
-  baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
-  jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  name: "Alvaro Salort",
+  jobDescription: "SOC Analyst",
+  about: `IT professional with Help Desk experience, focused on technical 
+  troubleshooting and user support. Currently looking to expand my career 
+  into cybersecurity as a SOC Analyst L1. I possess skills in system 
+  monitoring and analysis, with a strong commitment to security and data protection. 
+  Dedicated to continuous improvement and able to work effectively in a team.
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
-      endDate: "Present",
+      name: "Conectar Igualdad",
+      location: "San Martín, Mendoza, Argentina",
+      startDate: "May 2018",
+      endDate: "2020",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+        "Help Desk for a government programme.",
+        "- Provision of level 1 technical support to end-users, including hardware and software troubleshooting.",
+        "- Configuration and maintenance of IT equipment, software and networks.",
+        "- System monitoring and response to technical incidents, ensuring optimal availability and performance of IT services.",
+        "- Documentation of incidents and solutions in the ticketing system.",
+        "- Collaboration with the IT team to improve support processes and procedures.",
       ],
     },
 
     {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      name: "English teacher",
+      location: "Mendoza, Argentna",
+      startDate: "Mar 2021",
+      endDate: "Jun 2024",
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
+        "- Planning and implementation of English classes for various levels and ages.",
+        "- Assessment of students' progress and adjustment of teaching methods according to individual needs.",
+        "- Development of teaching materials and interactive resources to improve understanding and use of the English language.",
       ],
     },
 
-    {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
-      description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
-      ],
-    },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
+      name: "IESDyT N° 9-001 'Gral. José de San Martín' ",
+      location: "Mendoza, Argentina",
+      startDate: "2018",
       endDate: "2022",
       description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+        "English Teacher Training.",
       ],
     },
     {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
+      name: "Front-End Programming - Web Full Stack",
+      location: "ITMaster Academy",
+      startDate: "2023",
+      endDate: "2023",
+      description: ["HTML5, CSS (including Bootstrap and SASS), JavaScript (ECMAScript 2019), and React for creating dynamic applications. In addition, I have learned to work with Git, perform testing, and handle JSON data using fetch to query web services."],
     },
     {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
+      name: "Cybersecurity by Google",
+      location: "Coursera",
+      startDate: "2024",
+      endDate: "2024",
+      description: ["Skills in network and information security, problem solving, critical thinking, threat analysis and vulnerability assessment. Tools and platforms such as Bash, Chronicle, Linux, Python, Splunk, Suricata, SQL, tcpdump, VirusTotal and WireShark."],
+    },
+    {
+      name: "Cybersecurity DesafíoLatam",
+      location: "DesafíoLatam",
+      startDate: "2023",
+      endDate: "2024",
+      description: ["Learned how to install and configure Windows Server and Linux Server operating systems on enterprise networks, explored data networks, cloud technologies and information security. I also studied the basics of ethical hacking, including ethics, legal framework, and specific techniques and tools used in this practice."],
+    },
+  ],
+
+  skills: [
+    {
+      name: "Defensive Cybersecurity",
+      description: [
+        "- Cryptography: TripleDES, AES, RSA, Elliptic Curves",
+        "- Information Security: DLP, IRM",
+        "- Network Security: Firewall (Pfsense), IDS (SecurityOnion), IPS, VPN, Proxy",
+        "- Host Security: AV, EDR, XDR, HIDS",
+        "- Vulnerability Management",
+        "- Incident Response: Splunk, TheHive, Cortex",
+        "- Identity Management: SAML, OAuth",
+        "- Risk Management: STRIDE, MITRE ATT&CK"
+      ]
     },
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "mailto:uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    instagram: "https://www.instagram.com/alvaro_salort/",
+    twitter: "",
+    github: "https://github.com/alvarosrt",
+    email: "mailto:alvarosalort9@gmail.com",
+    linkedin: "https://www.linkedin.com/in/alvaro-salort",
   },
 
   projects: [
@@ -94,4 +104,6 @@ export const info = {
       liveUrl: "https://astro-portfolio-uzair.vercel.app/",
     },
   ],
-};
+
+  baseUrl: "https://www.alvarosrt.github.io", 
+}
